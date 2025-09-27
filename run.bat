f2e0b4e8ed5802d89216f482c16579d3917b4bd7
Ezert <ezert.mail@gmail.com>
2025-09-27 14:28:37 -0300

    refactoring: step 3

--- run.bat
+++ run.bat
@@ -2,9 +2,6 @@
 echo ===============================================
 echo           VAGABONDS ^& VALLEYS
 echo ===============================================
-echo.
-echo Iniciando...
-echo.
 cd /d "%~dp0\SKETCH"
 "C:\Program Files\Godot\Godot_v4.4.1-stable_win64.exe" --path . minimal_triangle.tscn
 pause
